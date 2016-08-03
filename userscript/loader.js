bc352a7a6c16f77baf01e2bd3efb257dea63367b
Shawak <maxi.nussbaum@googlemail.com>
2016-08-03 07:03:12 +0200

    remove debug messages

--- userscript/loader.js
+++ userscript/loader.js
@@ -17,23 +17,18 @@
             $('#userbaritems').append('<a href="/forum/profile.php?do=editoptions"><li>elite-cloud</li></a>');
 
             var that = this;
-            this.log('loading..');
             var elem = $('form[action="profile.php?do=updateoptions"]');
             if (!elem.length) {
-                this.log('could not find settings page.');
                 return;
             }
-            this.log('detected settings page.');
 
-            this.log('injecting script..');
             $.ajax({
                 url: 'http://localhost/elite-cloud/api/plugin',
                 dataType: 'jsonp',
             }).done(function (e) {
-                that.log('success');
                 elem.parent().prepend(e.data.script);
             }).fail(function (e, status, err) {
-                that.log('error, ' + status);
+                console.log(status);
             });
 
             var authKey = this.getAuthKey();
@@ -44,20 +39,14 @@
             $('#ec_message').text(str);
         },
 
-        log: function(str) {
-            console.log('elite-cloud: ' + str);
-        },
-
         login: function (authKey) {
-            this.log('Logging in with authKey: "' + authKey + '"');
             var that = this;
             $.ajax({
                 url: 'http://localhost/elite-cloud/api/authenticate/' + authKey,
                 dataType: 'jsonp',
             }).done(function (e) {
-                that.log('success');
                 that.loggedIn = e.success;
-                if(that.loggedIn) {
+                if (that.loggedIn) {
                     that.user = e.data.user;
                     that.userscripts = e.data.userscripts;
                     that.setMessage('Logged in as ' + that.user.name);
@@ -66,7 +55,7 @@
                 }
                 // TODO: Load Userscripts
             }).fail(function (e, status, err) {
-                that.log('error, '.status);
+                console.log('error, '.status);
             });
         }
 
