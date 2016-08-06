cdd6a0b4ce80157e0548f274a8d3464756c0ad30
Shawak <maxi.nussbaum@googlemail.com>
2016-08-06 12:31:39 +0200

    caching is made by smarty

--- backend/init.php
+++ backend/init.php
@@ -4,13 +4,7 @@ session_cache_limiter('private');
 session_cache_expire(180);
 session_start();
 
-// prevent api results from being cached,
-// could be moved into api.php to allow caching of frontend html
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
-
-date_default_timezone_set("Europe/Berlin");
+date_default_timezone_set('Europe/Berlin');
 
 require DIR_VENDOR . 'autoload.php';
 
