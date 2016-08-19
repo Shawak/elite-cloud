656247fcd68aedecce11b026a69d0f33e4d676e6
Shawak <maxi.nussbaum@googlemail.com>
2016-08-19 04:01:37 +0200

    must be public because of pdo reasons

--- backend/Database.php
+++ backend/Database.php
@@ -4,7 +4,7 @@ class Database extends PDO
 {
     private static $instance;
 
-    private function __construct($host, $datb, $user, $pass)
+    public function __construct($host, $datb, $user, $pass)
     {
         try {
             parent::__construct('mysql:host=' . $host . ';dbname=' . $datb . ';charset=utf8', $user, $pass, array(
